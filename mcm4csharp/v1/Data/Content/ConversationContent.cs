a7d7ab083b829cbd5f3b730bc1f7ae889cfd3a8f
AllyMarthaJ <alex@allym.me>
2022-01-07 20:06:51 +1100

    Add even more stuff before testing; API still down.

---
+++ mcm4csharp/v1/Data/Content/ConversationContent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace mcm4csharp.v1.Data.Content {
+	public struct ConversationContent {
+		[JsonPropertyName("recipients_ids")]
+		public uint[] RecipientsId { get; set; }
+
+		[JsonPropertyName("title")]
+		public string Title { get; set; }
+
+		[JsonPropertyName("message")]
+		public string Message { get; set; }
+	}
+}
+
